2b634ce61d25c8054e64872d56dedb4b76d3d355
hoanganh <hoanganh.tang0202@gmail.com>
2024-11-20 00:10:02 +0700

    chore: add JSAM + PrimeTween

---
+++ Assets/AudioEnums - AudioLibrary.cs
@@ -0,0 +1,11 @@
+    public enum AudioLibrarySounds {
+        GainPointSFX,
+        FlySFX,
+        HitSFX,
+        LoseSFX,
+        TapSFX
+    }
+    public enum AudioLibraryMusic {
+        MainMenuBGM,
+        PlayScreenBGM
+    }
